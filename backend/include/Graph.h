19100cfd4af4a351dd50485768f6f90cdfa44649
Ramkumar K R <krramkumar2006g@gmail.com>
2025-08-15 15:47:30 +0530

    implemented thread id

--- backend/include/Graph.h
+++ backend/include/Graph.h
@@ -4,17 +4,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct EmailEdge{
+    int to;
+    int weight ;
+    int thread_id;
+};
+
+struct Email{
+    int email_id;
+    int from;
+    int to;
+    int thread_id;
+    string subject;
+    string body;
+};
+
 class Graph{
     private:
-    vector<vector<int>> adj;
-    int numEmails;
+    vector<vector<EmailEdge>> adjList;
+    vector<vector<int>> adjMatrix;
+    vector<Email> emailStore;
+    int numUsers;
+    int nextEmailId;
+    int nextThreadId;
 
     public:
     Graph(int n);
-    void addEdge(int fromEmail,int toEmail);
+    void addEdge(int fromEmail,int toEmail,int threadId);
     vector<int> getNeighbours(int emialId);
     void BFS(int startEmail);
     void DFS(int startEmail);
+
+    void sendEmail(int from ,int to ,string subject, string body);
+    void replyEmail(int email_id,int from,int to,string body);
+    void forwardEmail(int email_id,int from,int to,string body);
+    void viewThread(int email_id);
+    void viewThreadDFS(int start_mail_id);
+    void dfsHelper(int email_id,vector<bool> visited);
+
+    //void printAdjMatrix();
+
+    void viewGraph(int email_id);
+    void viewThread(int thread_id);
+    int getNextThreadId();
 };
 
 #endif
--- backend/src/Graph.cpp
+++ backend/src/Graph.cpp
@@ -4,60 +4,180 @@ using namespace std;
 
 Graph::Graph(int n)
 {
-    numEmails=n;
-    adj.resize(n);
+    numUsers=n;
+    adjList.resize(n);
+    adjMatrix.resize(n,vector<int> (n,0));
+    nextEmailId=0;
+    nextThreadId=100;
 }
 
-void Graph::addEdge(int fromEmail,int toEmail)
+int Graph::getNextThreadId()
 {
-    adj[fromEmail].push_back(toEmail);
+    return nextThreadId++;
 }
 
-vector<int> Graph::getNeighbours(int EmailId)
+void Graph::addEdge(int fromEmail,int toEmail,int thread_id)
 {
-    return adj[EmailId];
-}
-
-void Graph::BFS(int startEmail)
-{
-    vector<bool> visited(numEmails,false);
-    queue<int> q;
-    visited[startEmail]=false;
-    q.push(startEmail);
-
-    while(!q.empty())
+    bool found=false;
+    for(auto &edge: adjList[fromEmail])
     {
-        int email=q.front();
-        q.pop();
-        cout<<email<<" ";
-
-        for(auto x:adj[email])
+        if (edge.to==toEmail && edge.thread_id==thread_id)
         {
-            if (!visited[x])
-            {
-                q.push(x);
-                visited[x]=true;
-            }
+            edge.weight++;
+            found=true;
+            break;
         }
     }
-    cout<<"\n";
+    if(!found)
+    {
+        adjList[fromEmail].push_back({toEmail,1,thread_id});
+    }
+    adjMatrix[fromEmail][toEmail]+=1;
+    
 }
 
-void Graph::DFS(int startEmail)
+void Graph::sendEmail(int from,int to,string subject, string body)
 {
-    vector<bool> visited(numEmails, false);
-
-    function<void(int)> DFSHELPER = [&](int email) {
-        visited[email] = true;
-        cout << email << " ";
-
-        for (auto x : adj[email]) {
-            if (!visited[x]) {
-                DFSHELPER(x);
-            }
-        }
-    };
-
-    DFSHELPER(startEmail);
-    cout << "\n";
+    int threadId=getNextThreadId();
+    addEdge(from,to,threadId);
+    emailStore.push_back({nextEmailId++,from,to,threadId,subject,body});
+    cout<<"Email sent.Thread ID:"<<threadId<<"\n";
 }
+
+void Graph:: replyEmail(int email_id,int from,int to,string body)
+{
+    for(auto  email:emailStore)
+    {
+        if(email.email_id==email_id)
+        {
+            addEdge(from,to,email.thread_id);
+
+            string reply_subject="RE: "+email.subject;
+            emailStore.push_back({
+                nextEmailId++,
+                from,
+                to,
+                email.thread_id,
+                reply_subject,
+                body
+            });
+        }
+        cout << "Reply sent to Email ID " << email_id 
+                 << " in Thread " << email.thread_id << "\n";
+        return;
+    }
+    cout << "Error: Email ID " << email_id << " not found.\n";
+}
+
+void Graph::forwardEmail(int email_id,int from,int to,string body)
+{
+    for(auto email:emailStore)
+    {
+        if(email.email_id==email_id)
+        {
+            addEdge(from,to,email.thread_id);
+
+            string fwd_sub="FWD: "+email.subject;
+            emailStore.push_back({
+                nextEmailId++,
+                from,
+                to,
+                email.thread_id,
+                fwd_sub,
+                body
+            });
+
+            cout<<"Email forwarded from Email ID"<<email_id
+                <<"in thread"<<email.thread_id<<"\n";
+        }
+
+    }
+}
+
+void Graph::viewThread(int email_id)
+{
+    int thread_id=-1;
+    
+    for(auto email:emailStore)
+    {
+        if(email.email_id==email_id)
+        {
+            thread_id=email.thread_id;
+            break;
+        }
+    }
+
+    if(thread_id==-1)
+    {
+        cout<<"Error: Email id"<<email_id<<"not found\n";
+        return ;
+    }
+
+    cout<<"Thread"<<thread_id<<":\n";
+    for(auto email:emailStore)
+    {
+        if(email.email_id==thread_id)
+        {
+            cout<<"Email ID: "<<email.email_id
+                << "| From"<<email.from<<"-> To"<<email.to
+                << "| Subject :"<<email.subject
+                << "| Body "<<email.body<<"\n";
+        }
+    }
+}
+
+// void Graph::BFS(int startEmail)
+// {
+//     vector<bool> visited(numUsers,false);
+//     queue<int> q;
+//     visited[startEmail]=false;
+//     q.push(startEmail);
+
+//     while(!q.empty())
+//     {
+//         int email=q.front();
+//         q.pop();
+//         cout<<email<<" ";
+
+//         for(auto x:adjList[email])
+//         {
+//             if (!visited[x])
+//             {
+//                 q.push(x);
+//                 visited[x]=true;
+//             }
+//         }
+//     }
+//     cout<<"\n";
+// }
+
+// void Graph::DFS(int startEmail)
+// {
+//     vector<bool> visited(numUsers, false);
+
+//     function<void(int)> DFSHELPER = [&](int email) {
+//         visited[email] = true;
+//         cout << email << " ";
+
+//         for (auto x : adjList[email]) {
+//             if (!visited[x]) {
+//                 DFSHELPER(x);
+//             }
+//         }
+//     };
+
+//     DFSHELPER(startEmail);
+//     cout << "\n";
+// }
+
+// void Graph::printAdjMatrix()
+// {
+//     for (int i = 0; i < numUsers; i++)
+//     {
+//         for (int j = 0; j < numUsers; j++)
+//         {
+//             cout << adjMatrix[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+// }
